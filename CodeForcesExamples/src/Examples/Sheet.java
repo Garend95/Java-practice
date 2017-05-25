620a9f06c48e1e3953d8d9246ad8cf6b599a7478
Garend95 <garend95@yahoo.com>
2017-05-26 02:17:19 +0400

    format1 and format2 declared

--- CodeForcesExamples/src/Examples/Sheet.java
+++ CodeForcesExamples/src/Examples/Sheet.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
  * Created by lev on 5/25/2017.
  */
 public class Sheet {
+    // This project copies the c# version of the code, found in the repository of CSharpening.
 
     Scanner userinput = new Scanner(System.in);
+
+    System.out.println("Please enter the spreadsheet coordinates in either format");    // For example row23col52 or AAB1234
+    String answer = userinput.readLine();
+
+    bool format1;   // ex: row48col24 or col79row23532
+    bool format2;   // ex: aaa67 or idg253
+
 }
